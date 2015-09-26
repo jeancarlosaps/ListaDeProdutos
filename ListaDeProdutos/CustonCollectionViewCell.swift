ada2a905cc4fd6d653ee4fa1291b880d9e646893
Jean Carlos <jeancarlosaps@yahoo.com.br>
2015-09-26 00:13:23 -0300

    AppTest
    Primeiro app de teste estudando a linguagem Swift 2.0

---
+++ ListaDeProdutos/CustonCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustonCollectionViewCell.swift
+//  ListaDeProdutos
+//
+//  Created by Jean Carlos on 22/09/15.
+//  Copyright © 2015 Jean Carlos. All rights reserved.
+//
+
+import UIKit
+
+class CustonCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var textView: UILabel!
+}
